c84911b3033c2cb555c596dea25cd7efe69c46a6
AlfathGelardi <109941239+Alf4thG3lardi@users.noreply.github.com>
2022-12-03 09:25:51 +0700

    Add files via upload

---
+++ Belajar-PHP/index.php
@@ -0,0 +1,24 @@
+<?php
+    $judul="bljr php";
+    $isi="smkn2bdr"
+    $jurusan="rekayasa perangkat lunak";
+    $kelas="11";
+    $nama="jhony";
+    echo $judul;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo $nama;
+        ?>
+    </h1>
+</body>
+</html>
